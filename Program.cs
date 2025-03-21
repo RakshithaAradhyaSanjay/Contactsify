055263515493c9056ad30d9e69d39ecd0c1358b9
RakshithaAradhyaSanjay <rakshitha.r75@gmail.com>
2025-03-21 17:34:16 +0800

    tyhgj

--- Program.cs
+++ Program.cs
@@ -31,6 +31,7 @@ namespace ContactsApp
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthorization();
 
